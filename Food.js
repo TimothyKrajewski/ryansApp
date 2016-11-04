613cd0d8419f0ced0fafbad1efd70109bba048c5
justinmeilinger <justinmeilinger62@gmail.com>
2016-11-03 23:09:00 -0400

    Modified the Food.js file and tested click event listners and they work correctly. need to work on differentiating between images. Possibly with a name atribute

--- Food.js
+++ Food.js
@@ -1,16 +1,29 @@
 (function(){
-  var food = {Type:"", Price:0.0,};
-  var cart = new Array(10);
 
-  function addToCart(type){
-    var i = new food();
-    i.Type = type;
-    cart.add(i);
+  var cart = new Array(10);
+  var images = new Array();
+
+  images = document.getElementsByClassName('img-thumbnail');
+  for (var i = 0; i < images.length; i++){
+    images[i].addEventListener("click", clickEvent);
+  }
+
+  function clickEvent(event){
+    alert(this.innerHTML);
+    addToCart("", 13.57);
+    alert("Item Successfully added");
+  }
+
+  function addToCart(type, price){
+    var food = {Type:"", Price:0.0,};
+    food.Type = type;
+    food.Price = price;
+    cart.push(food);
   }
 
   function getReceipt(){
     var str = "";
-    for(var i = 0; i < cart.length(); i++){
+    for(var i = 0; i < cart.length; i++){
       str += cart[i].Type + cart[i].Price;
     }
     return str;
