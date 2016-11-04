1190c34dedb15ddfde5b68b79b52e584a02a5c46
justinmeilinger <justinmeilinger62@gmail.com>
2016-11-03 22:20:24 -0400

    Added javascript file that theoretically can be used to store the current order. The file has not been tested yet

---
+++ Food.js
@@ -0,0 +1,19 @@
+(function() {
+  var food = {Type:"", Price:0.0,};
+  var cart = new Array(10);
+
+  function addToCart(var type){
+    var i = new food();
+    i.Type = type;
+    cart.add(i);
+  }
+
+  function getReceipt(){
+    var str = "";
+    for(var i = 0; i < cart.length(); i++){
+      str += cart[i].Type + cart[i].Price;
+    }
+    return str;
+  }
+
+})();
